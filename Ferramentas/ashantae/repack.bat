c3bea089cf38eb1afc467da75b9c413217fe7c03
Leomontenegro <leomontenegro6@gmail.com>
2025-08-30 20:32:46 -0300

    Inserido gráfico do gameover e do clear.

--- Ferramentas/ashantae/repack.bat
+++ Ferramentas/ashantae/repack.bat
@@ -1,5 +1,12 @@
 @echo off
 del .\310
+del .\623
+
 .\ashantae.exe tsx-reconstruct title_edited 310
+.\ashantae.exe tsx-reconstruct gameover_edited 623
+
 copy .\310 ..\..\Graficos\Editados\
+copy .\623 ..\..\Graficos\Editados\
+
 del .\310
+del .\623
--- Ferramentas/ashantae/unpack.bat
+++ Ferramentas/ashantae/unpack.bat
@@ -1,3 +1,6 @@
 @echo off
 rd /q /s title
+rd /q /s gameover
+
 .\ashantae.exe tsx-extract .\310_title .\308_title_scn title
+.\ashantae.exe tsx-extract .\623_gameover .\625_gameover_scn gameover
