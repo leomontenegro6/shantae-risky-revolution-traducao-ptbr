a48c3f75e65e3541e41dcf0c6fef400bc3f89d9e
Leomontenegro <leomontenegro6@gmail.com>
2025-08-30 23:10:04 -0300

    Editado background da tela de inventário, ao apertar Start.

--- Ferramentas/ashantae/repack.bat
+++ Ferramentas/ashantae/repack.bat
@@ -1,12 +1,16 @@
 @echo off
 del .\310
 del .\623
+del .\394
 
 .\ashantae.exe tsx-reconstruct title_edited 310
 .\ashantae.exe tsx-reconstruct gameover_edited 623
+.\ashantae.exe tsx-reconstruct status_edited 394
 
 copy .\310 ..\..\Graficos\Editados\
 copy .\623 ..\..\Graficos\Editados\
+copy .\394 ..\..\Graficos\Editados\
 
 del .\310
 del .\623
+del .\394
--- Ferramentas/ashantae/unpack.bat
+++ Ferramentas/ashantae/unpack.bat
@@ -1,6 +1,8 @@
 @echo off
 rd /q /s title
 rd /q /s gameover
+rd /q /s status
 
 .\ashantae.exe tsx-extract .\310_title .\308_title_scn title
 .\ashantae.exe tsx-extract .\623_gameover .\625_gameover_scn gameover
+.\ashantae.exe tsx-extract .\394_status .\391_status_scn status
