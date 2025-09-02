26e82629277988766f2fbfaf84b9257960f9a89e
Leomontenegro <leomontenegro6@gmail.com>
2025-09-02 09:47:03 -0300

    Edição preliminar do gráfico do inventário contendo nomes das danças.

--- Ferramentas/ashantae/repack.bat
+++ Ferramentas/ashantae/repack.bat
@@ -3,18 +3,21 @@ del .\310
 del .\623
 del .\394
 ::del .\398
+::del .\400
 
-.\ashantae.exe tsx-reconstruct title_edited 310
-.\ashantae.exe tsx-reconstruct gameover_edited 623
-.\ashantae.exe tsx-reconstruct status_edited 394
-::.\ashantae.exe tsx-reconstruct status_names_edited 398
+.\ashantae.exe tsx-reconstruct-no-lyr title_edited 310
+.\ashantae.exe tsx-reconstruct-no-lyr gameover_edited 623
+.\ashantae.exe tsx-reconstruct-no-lyr status_edited 394
+::.\ashantae.exe tsx-reconstruct status_names_edited 398 400
 
 copy .\310 ..\..\Graficos\Editados\
 copy .\623 ..\..\Graficos\Editados\
 copy .\394 ..\..\Graficos\Editados\
 ::copy .\398 ..\..\Graficos\Editados\
+::copy .\400 ..\..\Graficos\Editados\
 
 del .\310
 del .\623
 del .\394
 ::del .\398
+::del .\400
--- Ferramentas/ashantae/unpack.bat
+++ Ferramentas/ashantae/unpack.bat
@@ -4,7 +4,7 @@ rd /q /s gameover
 rd /q /s status
 rd /q /s status_names
 
-.\ashantae.exe tsx-extract .\310_title .\308_title_scn title
-.\ashantae.exe tsx-extract .\623_gameover .\625_gameover_scn gameover
-.\ashantae.exe tsx-extract .\394_status .\391_status_scn status
-.\ashantae.exe tsx-extract .\398_status_names .\391_status_scn status_names
+.\ashantae.exe tsx-extract-no-lyr .\310_title .\308_title_scn title
+.\ashantae.exe tsx-extract-no-lyr .\623_gameover .\625_gameover_scn gameover
+.\ashantae.exe tsx-extract-no-lyr .\394_status .\391_status_scn status
+.\ashantae.exe tsx-extract .\398_status_names .\400_status_lyr .\391_status_scn status_names
