6bdaacd6f23788e8afdd01728237ec193d3d0825
Leomontenegro <leomontenegro6@gmail.com>
2025-09-02 07:27:58 -0300

    Edição preliminar do gráfico do inventário contendo nomes das danças.

--- Ferramentas/ashantae/repack.bat
+++ Ferramentas/ashantae/repack.bat
@@ -2,15 +2,19 @@
 del .\310
 del .\623
 del .\394
+::del .\398
 
 .\ashantae.exe tsx-reconstruct title_edited 310
 .\ashantae.exe tsx-reconstruct gameover_edited 623
 .\ashantae.exe tsx-reconstruct status_edited 394
+::.\ashantae.exe tsx-reconstruct status_names_edited 398
 
 copy .\310 ..\..\Graficos\Editados\
 copy .\623 ..\..\Graficos\Editados\
 copy .\394 ..\..\Graficos\Editados\
+::copy .\398 ..\..\Graficos\Editados\
 
 del .\310
 del .\623
 del .\394
+::del .\398
--- Ferramentas/ashantae/unpack.bat
+++ Ferramentas/ashantae/unpack.bat
@@ -2,7 +2,9 @@
 rd /q /s title
 rd /q /s gameover
 rd /q /s status
+rd /q /s status_names
 
 .\ashantae.exe tsx-extract .\310_title .\308_title_scn title
 .\ashantae.exe tsx-extract .\623_gameover .\625_gameover_scn gameover
 .\ashantae.exe tsx-extract .\394_status .\391_status_scn status
+.\ashantae.exe tsx-extract .\398_status_names .\391_status_scn status_names
