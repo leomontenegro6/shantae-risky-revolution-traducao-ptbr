90029635dc29bbac54580b80c5a7d099f7f4a885
Leomontenegro <leomontenegro6@gmail.com>
2025-09-07 19:42:58 -0300

    Editado gráfico da tela "Destinations".

--- Ferramentas/ashantae/repack.bat
+++ Ferramentas/ashantae/repack.bat
@@ -2,22 +2,19 @@
 del .\310
 del .\623
 del .\394
-::del .\398
-::del .\400
+del .\632
 
 .\ashantae.exe tsx-reconstruct-no-lyr title_edited 310
 .\ashantae.exe tsx-reconstruct-no-lyr gameover_edited 623
 .\ashantae.exe tsx-reconstruct-no-lyr status_edited 394
-::.\ashantae.exe tsx-reconstruct status_names_edited 398 400
+.\ashantae.exe tsx-reconstruct-no-lyr destinations_edited 632
 
 copy .\310 ..\..\Graficos\Editados\
 copy .\623 ..\..\Graficos\Editados\
 copy .\394 ..\..\Graficos\Editados\
-::copy .\398 ..\..\Graficos\Editados\
-::copy .\400 ..\..\Graficos\Editados\
+copy .\632 ..\..\Graficos\Editados\
 
 del .\310
 del .\623
 del .\394
-::del .\398
-::del .\400
+del .\632
--- Ferramentas/ashantae/unpack.bat
+++ Ferramentas/ashantae/unpack.bat
@@ -2,9 +2,11 @@
 rd /q /s title
 rd /q /s gameover
 rd /q /s status
-rd /q /s status_names
+::rd /q /s costume_select
+rd /q /s destinations
 
 .\ashantae.exe tsx-extract-no-lyr .\310_title .\308_title_scn title
 .\ashantae.exe tsx-extract-no-lyr .\623_gameover .\625_gameover_scn gameover
 .\ashantae.exe tsx-extract-no-lyr .\394_status .\391_status_scn status
-.\ashantae.exe tsx-extract .\398_status_names .\400_status_lyr .\391_status_scn status_names
+::.\ashantae.exe tsx-extract-no-lyr .\329_costume_select .\328_costume_select_scn costume_select
+.\ashantae.exe tsx-extract-no-lyr .\632_destinations .\631_destinations_scn destinations
