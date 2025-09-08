b1ff3d03a25422b2cbb1f19a9a174f426a2068ff
Leomontenegro <leomontenegro6@gmail.com>
2025-09-08 08:51:58 -0300

    Added "Buy Stuff" files for future LYR experiments.

--- Ferramentas/ashantae/repack.bat
+++ Ferramentas/ashantae/repack.bat
@@ -4,11 +4,13 @@ del .\623
 del .\394
 del .\398
 del .\400
+del .\721
 
 .\ashantae.exe tsx-reconstruct-no-lyr title_edited 310
 .\ashantae.exe tsx-reconstruct-no-lyr gameover_edited 623
 .\ashantae.exe tsx-reconstruct-no-lyr status_edited 394
 .\ashantae.exe tsx-reconstruct status_names_edited 398 400
+.\ashantae.exe tsx-reconstruct-no-lyr buy_stuff_edited 721
 
 ::.\ashantaetest.exe tsx-reconstruct test 398
 ::.\ashantaetest.exe tsx-reconstruct --lyr 400 testlyr 398
@@ -18,9 +20,11 @@ copy .\623 ..\..\Graficos\Editados\
 copy .\394 ..\..\Graficos\Editados\
 copy .\398 ..\..\Graficos\Editados\
 copy .\400 ..\..\Graficos\Editados\
+copy .\721 ..\..\Graficos\Editados\
 
 del .\310
 del .\623
 del .\394
 del .\398
 del .\400
+del .\721
--- Ferramentas/ashantae/unpack.bat
+++ Ferramentas/ashantae/unpack.bat
@@ -3,6 +3,7 @@ rd /q /s title
 rd /q /s gameover
 rd /q /s status
 rd /q /s status_names
+rd /q /s buy_stuff
 
 .\ashantae.exe tsx-extract-no-lyr .\310_title .\308_title_scn title
 .\ashantae.exe tsx-extract-no-lyr .\623_gameover .\625_gameover_scn gameover
@@ -11,3 +12,4 @@ rd /q /s status_names
 
 ::.\ashantaetest.exe tsx-extract .\398_status_names .\391_status_scn test
 ::.\ashantaetest.exe tsx-extract --lyr .\400_status_lyr .\398_status_names .\391_status_scn testlyr
+.\ashantae.exe tsx-extract-no-lyr .\721_buy_stuff .\720_buy_stuff_scn buy_stuff
